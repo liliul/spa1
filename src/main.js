def615133d65e26cd46721bbe440fb03d8a96795
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-03 18:50:18 -0300

    update: main.js index.html

--- src/main.js
+++ src/main.js
@@ -1,23 +1,41 @@
-import Brasil from './pages/home/brasil.js';
+/**import Brasil from './pages/home/brasil.js';
 import Alemanha from './pages/home/alemanha.js';
 import Reino from './pages/home/reino.js';
 import Usa from './pages/home/usa.js';
 import Erro from './pages/home/erro.js';
-
-
+import Home from './pages/home/home.js';
 
 
 const root = document.querySelector('#root')
 
-const iniciar = () => {
-    window.addEventListener('hashchange', () => {
-        console.log(window.location.hash)
+// const hash = (hash) => hash === "" ? 'home' : hash.replace('#', '');
+// const url = hash(window.location.hash);
+// console.log('url', url)
+// root.appendChild(url)
 
+
+const iniciar = () => {
+    // if(window.location.pathname === '/'){
+    //     root.append(Home())
+    // }
+    // if(window.location.pathname == '/d'){
+    //     root.append(Usa())
+    // }
+    
+    window.addEventListener('hashchange', () => {
+        // console.log(window.location.hash.substring(1))
         root.innerHTML = "";
 
+       
+        
+        
+        
         switch(window.location.hash) {
+            case "#home":
+                root.appendChild(Home())
+                break
             case "#brasil":
-                root.appendChild(Brasil())
+                root.append(Brasil())
                 break
             case "#alemanha":
                 root.appendChild(Alemanha())
@@ -35,8 +53,45 @@ const iniciar = () => {
     })
 }
 
-window.addEventListener('load', () => {
-    root.appendChild(Brasil())
 
-    iniciar()
+
+
+window.addEventListener('load', () => {
+    //root.appendChild(Brasil())
+    // validar();
+    iniciar();
+})
+*/
+
+import Brasil from './pages/home/brasil.js';
+import Alemanha from './pages/home/alemanha.js';
+import Reino from './pages/home/reino.js';
+import Usa from './pages/home/usa.js';
+import Erro from './pages/home/erro.js';
+import Home from './pages/home/home.js';
+
+
+const routes = {
+    brasil: Brasil(),
+    alemanha: Alemanha(),
+    reino: Reino(),
+    eua: Usa(),
+    error: Erro(),
+    home: Home()
+}
+
+const root = document.querySelector('#root')
+
+const iniciar = () => window.addEventListener('hashchange', renderPaginas) 
+const hash = (hash) => hash === "" ? 'home' : hash.replace('#', '');
+
+function renderPaginas() {
+    const verPagina = hash(window.location.hash);
+    root.innerHTML = '';
+    root.appendChild(routes[verPagina]);
+
+}
+window.addEventListener('load', () => {
+    renderPaginas();
+    iniciar();
 })
