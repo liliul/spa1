1cf4199cef674315f3f5825d678a320bd83026db
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-03 18:49:44 -0300

    chore: reino.js

--- src/pages/home/reino.js
+++ src/pages/home/reino.js
@@ -1,21 +1,34 @@
+function text(){return 'function text return'}
+function Somar(num1,num2){
+    return console.log(num1 + num2);
+}
+function* gerar(){
+    console.log('inicio do yield')
+    yield 'messagem gerandora!!!'
+    console.log('fim do yield')
+}
+
 export default () => {
     const divImg = document.createElement('div')
 
     const content = `
         <section class="content">
             <div class="img">
-                <img src="./src/assets/ingraterra.svg" alt="ingraterra">
+                <a href='#brasil'>
+                    <img src="./src/assets/ingraterra.svg" alt="ingraterra">
+                </a>    
             </div>
 
             <article class="ler">
                 <h1>Lorem input cat spas</h1>
-
+                
+                <strong onclick='${Somar(10,10)}' style='font-size:15px;'>${text()}</strong>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates magnam tenetur sit deleniti magni perspiciatis voluptas aliquam, nesciunt laboriosam natus impedit. Quaerat iusto aliquam excepturi accusantium harum aspernatur ducimus possimus.</p>
             </article>
         </section>
     `
 
     divImg.innerHTML = content;
-
+    console.log(gerar().next())
     return divImg;
 }
