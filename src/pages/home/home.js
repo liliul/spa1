eb2feae7979fae506212569b48f6f47805c5eead
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-03 12:56:08 -0300

    create: home.js

---
+++ src/pages/home/home.js
@@ -0,0 +1,18 @@
+export default () => {
+	const divHome = document.createElement('div')
+
+	divHome.innerHTML = `
+		<section class="content">
+            <div class="img">
+                <img src="./src/assets/brasil.svg" alt="brasil">
+            </div>
+
+            <article class="ler">
+                <h1>Lorem input cat spas</h1>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates magnam tenetur sit deleniti magni perspiciatis voluptas aliquam, nesciunt laboriosam natus impedit. Quaerat iusto aliquam excepturi accusantium harum aspernatur ducimus possimus.</p>
+            </article>
+        </section>
+	`
+	return divHome;
+}
