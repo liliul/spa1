977ef456a45b7092bc66390f0ab3d774b89c8629
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-03 18:45:37 -0300

    update: home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,11 +4,11 @@ export default () => {
 	divHome.innerHTML = `
 		<section class="content">
             <div class="img">
-                <img src="./src/assets/brasil.svg" alt="brasil">
+                <img src="./src/assets/react-2.svg" alt="brasil">
             </div>
 
             <article class="ler">
-                <h1>Lorem input cat spas</h1>
+                <h1>Home</h1>
 
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates magnam tenetur sit deleniti magni perspiciatis voluptas aliquam, nesciunt laboriosam natus impedit. Quaerat iusto aliquam excepturi accusantium harum aspernatur ducimus possimus.</p>
             </article>
